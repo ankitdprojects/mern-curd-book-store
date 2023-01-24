4ec73070da0e9ba3fa04ee76ea3a5d0a45d3c25b
Ankit <102902873+ankitdprojects@users.noreply.github.com>
2023-01-24 13:01:54 +0530

    added dot env mongo connection

--- Backend/Connenction/Connection.js
+++ Backend/Connenction/Connection.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 
-mongoose.connect('mongodb+srv://ankitbook:ADD224898@cluster0.blsmir1.mongodb.net/BookStore?retryWrites=true&w=majority')
-.then(() => console.log('Connected') )
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.blsmir1.mongodb.net/BookStore?retryWrites=true&w=majority`)
+.then(() => console.log('Connected') );
