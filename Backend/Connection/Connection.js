30ba320542b22b7161a1b52972d769fea15a300d
Ankit <102902873+ankitdprojects@users.noreply.github.com>
2023-01-24 16:53:24 +0530

    trying again

--- Backend/Connection/Connection.js
+++ Backend/Connection/Connection.js
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PA7SSWORD}@cluster0.blsmir1.mongodb.net/BookStore?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.blsmir1.mongodb.net/BookStore?retryWrites=true&w=majority`)
 .then(() => console.log('Connected') );
