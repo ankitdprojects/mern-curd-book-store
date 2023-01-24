4f6309e93fc5b99b7084007a63f10cd9896060a8
Ankit <102902873+ankitdprojects@users.noreply.github.com>
2023-01-24 12:45:47 +0530

    added dot env

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import './Connenction/Connection.js';
 import router from './Connenction/Routes/bookRoutes.js';
 import cors from 'cors';
+import dotenv from "dotenv";
 
+dotenv.config();
 var app=express();
 app.use(express.json())
 app.use(cors())
 app.use('/api/v1',router)
 
-app.listen(3000 , () => {
+app.listen(process.env.PORT || 3000 , () => {
     console.log('SERVER STARTED')
 })
